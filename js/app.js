6d9eb4e4430fdebdc5812523c0843f24bf9c0518
Marcin Trawiński <trawinski1@wp.pl>
2018-03-27 19:55:40 +0200

    Code refactored to es6 classes

--- js/app.js
+++ js/app.js
@@ -1,59 +1,60 @@
-// Book constructor
-function Book(title, author, isbn) {
-  this.title  = title;
-  this.author = author;
-  this.isbn = isbn;
-}
-
-//UI constructor
-function UI() {
-
-}
-//Add book to list
-UI.prototype.addBookToList = function (book) {
-  const list = document.querySelector('#book-list');
-  //Create new row in table
-  const row = document.createElement('tr');
-
-  row.innerHTML = `<td>${book.title}</td>
-                  <td>${book.author}</td>
-                  <td>${book.isbn}</td>
-                  <td class="delete-td"><span class="delete">X</span></td>`;
-
-  // Append new row to table                  
-  list.appendChild(row);
-} 
-
-UI.prototype.clearFormFields = function() {
-  const formFields = document.querySelectorAll('.form-input');
-  
-  formFields.forEach(field => {
-    field.value = '';
-  })
-}
-
-// Show message
-UI.prototype.showMessage = function(message, cssClass) {
-  const messageBox = document.querySelector('#message-box');
-  //Create message element
-  const div = document.createElement('div');
-  div.className = `message ${cssClass}`;
-  div.innerText = message;
-
-  messageBox.appendChild(div);
-  //Remove message after 3s.
-  setTimeout(() => {
-    messageBox.removeChild(div);
-  }, 3000);
-}
-
-UI.prototype.deleteBook = function (target) {
-  if (target.className === 'delete') {
-    target.parentElement.parentElement.remove();
+// Book class
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
   }
 }
 
-// instatniate UI object
+// UI class
+class UI {
+  addBookToList(book) {
+    const list = document.querySelector('#book-list');
+    //Create new row in table
+    const row = document.createElement('tr');
+
+    row.innerHTML = `<td>${book.title}</td>
+                     <td>${book.author}</td>
+                     <td>${book.isbn}</td>
+                     <td class="delete-td"><span class="delete">X</span></td>`;
+
+    // Append new row to table                  
+    list.appendChild(row);
+  }
+
+  clearFormFields() {
+    const formFields = document.querySelectorAll('.form-input');
+
+    formFields.forEach(field => {
+      field.value = '';
+    });
+  }
+
+  showMessage(message, cssClass) {
+    const messageBox = document.querySelector('#message-box');
+    //Create message element
+    const div = document.createElement('div');
+    div.className = `message ${cssClass}`;
+    div.innerText = message;
+    
+    //Append message element to message box
+    messageBox.appendChild(div);
+
+    //Remove message after 3s.
+    setTimeout(() => {
+      messageBox.removeChild(div);
+    }, 3000);
+  }
+
+  deleteBook(target) {
+    if (target.className === 'delete') {
+      target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+// Instantiate UI object
 const ui = new UI();
 
 
@@ -64,28 +65,29 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
   const author = document.querySelector('#author').value;
   const isbn = document.querySelector('#isbn').value;
 
-  // Instatiate book
+  // Instantiate book
   const book = new Book(title, author, isbn);
 
   //Validate form fields
 
   if (title === '' || author === '' || isbn === '') {
-    ui.showMessage('Please fill in all fields', 'error');
+    // Show appropriate message
+    ui.showMessage('Please fill in all fields!', 'error');
   } else {
     //Add book to list
     ui.addBookToList(book);
     //Clear form fields
     ui.clearFormFields();
-
+    
     ui.showMessage('Book added', 'success');
   }
 
-  e.preventDefault()
+  e.preventDefault();
 });
 
 // Event listenter for remove book
 document.querySelector('#book-list').addEventListener('click', (e) => {
   ui.deleteBook(e.target);
-  ui.showMessage('Book deleted', 'success');
+  ui.showMessage('Book deleted!', 'success');
   e.preventDefault();
-})
+});

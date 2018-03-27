de3756ef743a4322b3bccbfe85d17e2fe4037eeb
Marcin Trawiński <trawinski1@wp.pl>
2018-03-27 21:27:25 +0200

    Add local storage persistence

--- js/app.js
+++ js/app.js
@@ -46,7 +46,7 @@ class UI {
       messageBox.removeChild(div);
     }, 3000);
   }
-
+  
   deleteBook(target) {
     if (target.className === 'delete') {
       target.parentElement.parentElement.remove();
@@ -57,6 +57,55 @@ class UI {
 // Instantiate UI object
 const ui = new UI();
 
+// Local Storage class
+
+class LStorage {
+  //Get books from LS
+  static getBooks() {
+    let books;
+    //Check if there are any books in LS
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+
+    return books;
+  }
+
+  //Add books from LS to table
+  static displayBooks() {
+    const books = LStorage.getBooks();
+    
+    books.forEach(book => {
+      ui.addBookToList(book);
+    })
+  }
+
+  //Add book to Local Storage
+  static addBookToLS(book) {
+    const books = this.getBooks();
+    books.push(book);
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  //Remove book from Local Storage
+  static removeBookFromLS(isbn) {
+    const books = this.getBooks();
+    books.forEach((book, index) => {
+      if (book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    })
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
+
+//Gets books from Local Storage after page load
+document.addEventListener('DOMContentLoaded', LStorage.displayBooks);
 
 // Event listenter for add book
 document.querySelector('#book-form').addEventListener('submit', (e) => {
@@ -76,6 +125,10 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
   } else {
     //Add book to list
     ui.addBookToList(book);
+
+    //Add to Local Storage
+    LStorage.addBookToLS(book);
+
     //Clear form fields
     ui.clearFormFields();
     
@@ -87,7 +140,12 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
 
 // Event listenter for remove book
 document.querySelector('#book-list').addEventListener('click', (e) => {
+  //Remove book from list
   ui.deleteBook(e.target);
+
+  //Remove book from Local Storage
+    //Gets isbn field text conttent
+  LStorage.removeBookFromLS(e.target.parentElement.previousElementSibling.textContent);
   ui.showMessage('Book deleted!', 'success');
   e.preventDefault();
 });
